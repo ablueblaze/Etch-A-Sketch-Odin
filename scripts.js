50e89203ac321f6d0651568b6c66585f76883883
ablueblaze <blazeascott@gmail.com>
2021-10-07 02:54:27 -0500

    Solved the overflow issue by adjusting the width to fit-content.

--- scripts.js
+++ scripts.js
@@ -1,19 +1,19 @@
 function makeGrid(spots) {
-    const container = document.getElementById("grid");
-    container.textContent = '';
-    for (let i = 0; i < spots; i++) {
-        let colum = document.createElement('div');
-        colum.className = "colum"
-        container.append(colum);
-        for (let n = 0; n < spots; n++) {
-            let div = document.createElement('div');
-            div.style.padding = `${500 / (spots*2)}px`;
-            div.onmouseover = function(){
-                div.setAttribute("class", "color");
-            }
-            colum.append(div);
-        }
+  const container = document.getElementById("grid");
+  container.textContent = "";
+  for (let i = 0; i < spots; i++) {
+    let colum = document.createElement("div");
+    colum.className = "colum";
+    container.append(colum);
+    for (let n = 0; n < spots; n++) {
+      let div = document.createElement("div");
+      div.style.padding = `${500 / (spots * 2)}px`;
+      div.onmouseover = function () {
+        div.setAttribute("class", "color");
+      };
+      colum.append(div);
     }
+  }
 }
 
 makeGrid(50);
@@ -21,103 +21,21 @@ makeGrid(50);
 let clear = document.getElementById("clear");
 let gridSize = document.getElementById("gridSize");
 let outputValue = document.getElementById("value");
-let para = document.createElement('p');
+let para = document.createElement("p");
 
 function sizeOfGrid() {
-    let currentVal = gridSize.value;
-    return currentVal;
+  let currentVal = gridSize.value;
+  return currentVal;
 }
 
 outputValue.textContent = sizeOfGrid();
 
+gridSize.addEventListener("change", function (event) {
+  outputValue.textContent = "";
+  outputValue.textContent = sizeOfGrid();
+  makeGrid(sizeOfGrid());
+});
 
-// outputValue.addEventListener('input', function (event) {
-//     outputValue.textContent = '';
-//     outputValue.append(sizeOfGrid());
-
-// })
-
-gridSize.addEventListener('change', function (event) {
-    outputValue.textContent = '';
-    outputValue.textContent = sizeOfGrid();
-    makeGrid(sizeOfGrid());
-})
-
-clear.addEventListener('click', function (event) {
-    makeGrid(sizeOfGrid());
-})
-
-
-// function reset() {
-//     document.getE
-// }
-
-
-
-
-
-
-
-
-
-/////////////////// Kinda works, but not really.
-// const shift = document.querySelectorAll(".button");
-
-// function test(){
-    //     console.log("test");
-    //     // shift.classList.remove('color');
-    //     // shift.classList.add('changedColor');
-    // }
-    
-    // shift.forEach(item => {
-        //     item.addEventListener('mouseover', event => {
-            //         test();
-            //         document.shift.classList.add('test')
-            //     })
-            // });
-            /////////////////
-            
-            
-            
-            
-            
-// div.onmouseover = function() {
-//     document.getElementsByTagName('div')[-1].classList.add('color');
-// }
-            
-            
-            
-// I need to assign an ID to each of the buttons.
-
-// document.querySelector('.button').cssText = "padding: 5px;";
-
-// shift.addEventListener('click', function (event) {
-    //     test();
-// })
-
-// shift.onmouseover = test();
-
-// for (let i = 0; i < 16; i++){
-//     const div = document.createElement('div');
-//     div.classList.add('colum');
-//     const node = document.createTextNode("test");
-//     div.appendChild(node);
-
-//     element.innerHTML = div
-    
-//     const container = document.getElementById('container');
-//     container.appendChild(div);
-
-    // for (let n = 0; n < 16; i++){
-    //     const div2 = document.createElement('div');
-    //     div2.classList.add('row')
-    //     const node2 = document.createTextNode("test");
-    //     div2.appendChild(node2);
-        
-    // }
-// }
-
-// let sheet = document.createElement('style');
-// sheet.innerHTML = "div {border-style: solid; border - color: black; border - width: 1px;}"; 
-// sheet.innerHTML = ".container {display: grid; grid-template-columns: 50% 50%;}";
-// document.body.appendChild(sheet)
+clear.addEventListener("click", function (event) {
+  makeGrid(sizeOfGrid());
+});
